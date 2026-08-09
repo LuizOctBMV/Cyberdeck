0e4a07fe292d1bd2fc3c43a7ba4ce5b321b721f1
Luiz Octávio <luizoctavio.bml@gmail.com>
2026-08-09 20:26:28 -0300

    Conclusion of the TCP scan, elimination and inclusion of global variables into parameters. Expanding to OS fingerprinting

--- portscan.py
+++ portscan.py
@@ -2,6 +2,7 @@ import socket
 import struct
 from concurrent.futures import ThreadPoolExecutor
 import random 
+from functools import partial
 
 def create_TCP_header( #takes only int values
         source_port,
@@ -129,8 +130,7 @@ def evaluate_flags(flag):
         return "Closed"
 
 
-
-def create_syn_packet(port) : 
+def create_syn_packet(port, destination_ip, source_ip) : 
 
     '''
     random values for sequence, window and source port
@@ -139,6 +139,7 @@ def create_syn_packet(port) :
     seq_number = random.randint(1, 4294967295)  
     window_size = random.randint(62727, 65534)  
     src_port = random.randint(5000, 62439)      
+
 
     tcp_header = create_TCP_header(source_port=src_port, destination_port=port, sequence_number=seq_number, window_size=window_size)
     syn_pseudo_header = calculate_pseudo_header(source_ip=source_ip, destination_ip=destination_ip)
@@ -155,95 +156,154 @@ def create_syn_packet(port) :
 
     return [final_packet,src_port]
 
-def socket_dealing(port): 
+def syn_incomplete_connection(port, destination_ip, source_ip, quietness): 
     port_n_state = [port, "n/a"]
 
-    lst_syn_packlet = create_syn_packet(port)
+    lst_syn_packlet = create_syn_packet(port, destination_ip, source_ip)
 
     raw_socket = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
-    raw_socket.settimeout(3)
-    raw_socket.sendto(lst_syn_packlet[0], (destination_ip, port))
+    raw_socket.settimeout(quietness[0])
+    
 
     sender_src_ip = ""                      #the analyzed packet must have the same source ip as the destination one from the first packet, so the packet received is indeed the one expected
     sender_src_port = 0
     sender_destination_port = 0
     tcp_recv = 0x0
 
-    while (sender_src_ip != destination_ip) or (sender_src_port != port) or (sender_destination_port != lst_syn_packlet[1]):
-        try: 
-            received_data = raw_socket.recv(65535)
+    try: 
+        raw_socket.sendto(lst_syn_packlet[0], (destination_ip, port))
+        while (sender_src_ip != destination_ip) or (sender_src_port != port) or (sender_destination_port != lst_syn_packlet[1]):
+            try: 
+                received_data = raw_socket.recv(65535)
 
-            first_byte = received_data[0]           # first byte contains version and IP packet length
-            ihl = first_byte & 0x0F                 # takes the version out
-            ip_header_size = ihl * 4 
-            tcp_recv = received_data[ip_header_size:]
+                first_byte = received_data[0]           # first byte contains version and IP packet length
+                ihl = first_byte & 0x0F                 # takes the version out
+                ip_header_size = ihl * 4 
+                tcp_recv = received_data[ip_header_size:]
 
 
-            sender_src_ip = socket.inet_ntoa(received_data[12:16])  # getting the source ip from the received ip header
-            sender_src_port = struct.unpack("!H", tcp_recv[0:2])[0]                   # getting the source port from the received tcp header
-            sender_destination_port = struct.unpack("!H", tcp_recv[2:4])[0]                   # getting the destination port from the received tcp header
+                sender_src_ip = socket.inet_ntoa(received_data[12:16])  # getting the source ip from the received ip header
+                sender_src_port = struct.unpack("!H", tcp_recv[0:2])[0]                   # getting the source port from the received tcp header
+                sender_destination_port = struct.unpack("!H", tcp_recv[2:4])[0]                   # getting the destination port from the received tcp header
 
-        except socket.timeout:
-            port_n_state[1] = "TimedOut"
-            return port_n_state 
-        finally:
-            raw_socket.close()   
+            except socket.timeout:
+                port_n_state[1] = "TimedOut"
+                return port_n_state 
+    finally:
+        raw_socket.close()   
 
     port_n_state = [port, evaluate_flags(tcp_recv[13])]
     
     return port_n_state
 
-def syn_scan(ports_to_be_scanned): 
+
+def syn_scan(ports_to_be_scanned, destination_ip, quietness, scan_display, source_ip):
+
+    sic_w_IP = partial(syn_incomplete_connection, destination_ip=destination_ip, quietness=quietness, source_ip=source_ip) # allows the IP address and other parameters to be included on the threading process
+    with ThreadPoolExecutor(max_workers=quietness[1]) as executor:
+        results = executor.map(sic_w_IP, ports_to_be_scanned)
+
+    printing_results(results, scan_display)
+
+def tcp_complete_connection(port, destination_ip, quietness): 
+
+    port_n_state = [port, "n/a"]
+
+    tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcp_socket.settimeout(quietness[0])
+
+    try: 
+        tcp_socket.connect((destination_ip, port))
+        port_n_state[1] = "Open"
+
+    except ConnectionRefusedError:
+        port_n_state[1] = "Closed"
+        
+    except socket.timeout:
+        port_n_state[1] = "TimedOut"
+
+    finally: 
+        tcp_socket.close()
+
+    return port_n_state
+
+
+def tcp_scan(ports_to_be_scanned, destination_ip, scan_quietness, scan_display): 
+
+    tcc_w_IP = partial(tcp_complete_connection, destination_ip=destination_ip, quietness=scan_quietness) # allows the IP address and other parameters to be included on the threading process
+    with ThreadPoolExecutor(max_workers=scan_quietness[1]) as executor:
+            results = executor.map(tcc_w_IP, ports_to_be_scanned)
     
+    printing_results(results, scan_display)
 
-    with ThreadPoolExecutor(max_workers=2) as executor:
-        results = executor.map(socket_dealing, ports_to_be_scanned)
-
+def printing_results(results, scan_display): 
 
     print("port     state")
-
+    
     amountClosed = 0
     amountTimedOut = 0
-
+    
     for result in results: 
         port_n = result[0]
         port_state = result[1]
-
+    
         if port_state == "Open": 
             print(f"{port_n}  open")
-        elif port_state == "Closed": 
-            amountClosed += 1
         elif port_state == "TimedOut": 
-            amountTimedOut += 1    
-
-    print(f"{amountClosed} ports closed and {amountTimedOut} ports timed out")
-
-
-destination_ip = input("Target's IP Address: ")
-source_ip = get_source_ip(destination_ip)
-global scan_quietness
-
-scan_type = int(input("Choose the corresponding value to your interest\n1. TCP-SCAN\n2. SYN-SCAN\n3. OS-FINGERPRINT"))
-
-if scan_type == 3:
-    #todo OS fingerprint
-
-else: 
-    
-    scan_quietness = int(input("How fast do you want the scan to be? \n"))
-    scan_pattern = int(input("Choose the corresponding value to your interest\n1. Scan on well known ports (0-1024)\n2. Complete Scan (0-65535)\n"))
-
-    if scan_pattern == 1: 
-        ports_to_be_scanned = range(1,1025)
-    elif scan_pattern == 2: 
-        ports_to_be_scanned = range(1,65536)
-
-    if scan_type == 1: 
-        #todo TCP full SCAN
+            amountTimedOut += 1
+        else:
+            if scan_display == 1: 
+                amountClosed += 1
+            else: 
+                print(f"{port_n}  closed")
+            
+            
+    if scan_display == 1:
+        print(f"{amountClosed} ports closed and {amountTimedOut} ports timed out")
     else: 
-        syn_scan(ports_to_be_scanned)
+        print(f"{amountTimedOut} ports timed out")
+
+def portscanning():
+    destination_ip = input("Target's IP Address: ")
+
+    print("Type only the number indicated by the alternative")
+    scan_type = int(input("Choose the corresponding value to your interest\n1. TCP-SCAN\n2. SYN-SCAN\n3. OS-FINGERPRINT"))
+
+    if scan_type == 3:
+        #todo OS fingerprinting
+        print("#todo")
+
+    else: 
+        
+        scan_quietness = int(input("How fast do you want the scan to be (scale 1-4, 4 being fast and 1 slower)?\n1. Slow\n2.Normal \n3.Fast \n4.Ultra Fast\n"))
+
+        # timeout / number of workers
+        if scan_quietness == 1:  
+            quietness = [4,50]
+        elif scan_quietness == 2:
+            quietness = [3,100]
+        elif scan_quietness == 3:
+            quietness = [1.5,250]
+        elif scan_quietness == 4:
+            quietness = [0.75,350]
+
+        scan_pattern = int(input("Choose the corresponding value to your interest\n1. Scan on well known ports (0-1024)\n2. Complete Scan (0-65535)\n"))
+
+        if scan_pattern == 1: 
+            ports_to_be_scanned = range(1,1025)
+        elif scan_pattern == 2: 
+            ports_to_be_scanned = range(1,65536)
 
 
+        scan_display = int(input("Display results\n1. Only opened ports \n2. Opened and closed ports\n"))
+        
+        if scan_type == 1: 
+            tcp_scan(ports_to_be_scanned, destination_ip, quietness, scan_display)
+        else: 
+            source_ip = get_source_ip(destination_ip)
+            syn_scan(ports_to_be_scanned, destination_ip, quietness, scan_display, source_ip)
+
+portscanning()
 
 
 
